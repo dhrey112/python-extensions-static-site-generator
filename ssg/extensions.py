128b5b2646dfb1d84c27cd8e8be07e71ff39a15f
dhrey <deeprince2020@gmail.com>
2022-02-06 17:48:10 +0100

    Update extensions

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -9,7 +9,7 @@ def load_module(directory, name):
 
 
 def load_directory(directory):
-    for path in directory.rglob('.py'):
+    for path in directory.rglob('*.py'):
         load_module(directory.as_posix(), path.stem)
 
 def load_bundled():
