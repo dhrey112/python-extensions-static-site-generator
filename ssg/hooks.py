0f5a9781e16476abb160dfb81ecbbb12802e2778
dhrey <deeprince2020@gmail.com>
2022-02-06 18:14:19 +0100

    Update Event and Filter Hooks

--- ssg/hooks.py
+++ ssg/hooks.py
@@ -5,6 +5,7 @@ def register(hook, order=0):
     def register_callback(func):
         _callbacks.setdefault(hook, {}).setdefault(order, []).append(func)
         return func
+    return register_callback
 
 
 def event(hook, *args):
@@ -16,5 +17,5 @@ def event(hook, *args):
 def filter(hook, value, *args):
     for order in sorted(_callbacks.get(hook, {})):
         for func in _callbacks[hook][order]:
-            value = func(value, args)
+            value = func(value, *args)
     return value
